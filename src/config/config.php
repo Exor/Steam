fd58435a6e178b23af006cc57b66b999e12ef425
Jon <jonathan.slabaugh@gmail.com>
2015-01-26 06:29:55 +0000

    New flags for game integration testing and app id

--- src/config/config.php
+++ src/config/config.php
@@ -7,4 +7,18 @@ return array(
 	 * Once you get your key, add it here.
 	*/
 	'steamApiKey' => 'YOUR-API-KEY',
+
+	/**
+	 * You can get a steam App Id from http://steamcommunity.com/
+	 * Once you get your id, add it here.
+	 * An App Id is necessary if your game uses microtransactions, web authentication, or Steam economy features
+	*/
+	'appId' => 'YOUR-APP-ID',
+
+	/**
+	 * If true, the microtransaction functions will hit the Steam test server.
+	 * Microtransaction orders are also stored in a test database instead of production.
+	 * Toggle this flag to false when your game goes live.
+	*/
+	'testEnvironment' => 'true',
 );
