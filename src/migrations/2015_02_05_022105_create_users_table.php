86ab3d06e9947f0fd918de3988c1314843e6fbaf
Jon <jonathan.slabaugh@gmail.com>
2015-02-05 02:39:55 +0000

    Remove unused models. Add users table.

---
+++ src/migrations/2015_02_05_022105_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	    Schema::create('steamapi_users', function($table)
+	    {
+			$table->bigIncrements('id');
+			$table->bigInteger('steamid')->unsigned();
+
+		    $table->timestamps();
+	    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('steamapi_users');
+	}
+
+}
